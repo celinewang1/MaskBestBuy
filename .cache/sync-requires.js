9f0e83d711178853696a3c370cc424b52986f547
Celine Wang <celinewanger@gmail.com>
2020-08-10 13:46:18 -0700

    styles

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/celinewang/Documents/github/MaskBestBuy/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/celinewang/Documents/github/MaskBestBuy/src/pages/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/celinewang/Documents/github/MaskBestBuy/src/pages/index.js"))),
+  "component---src-pages-styles-js": hot(preferDefault(require("/Users/celinewang/Documents/github/MaskBestBuy/src/pages/styles.js")))
 }
 
