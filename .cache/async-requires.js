c93f6295f3fc5371d4395f4528dd6b7e57a0816c
Kevin Y. Chung <kevinychung@Kevins-MacBook-Pro-2.local>
2020-08-11 09:39:00 +0900

    Revert "styles"
    This reverts commit 9f0e83d711178853696a3c370cc424b52986f547.

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-styles-js": () => import("./../src/pages/styles.js" /* webpackChunkName: "component---src-pages-styles-js" */)
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
